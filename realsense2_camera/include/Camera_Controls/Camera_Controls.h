83c728bbc4734d0e07f1abf36ed95b1f83c857dc
tkurc1 <humanoidgithub@gmail.com>
2021-04-23 16:20:12 +0800

    Let the depth and color stream resolution to 640*480
    but fail

--- realsense2_camera/include/Camera_Controls/Camera_Controls.h
+++ realsense2_camera/include/Camera_Controls/Camera_Controls.h
@@ -5,30 +5,37 @@
 #include <utility>
 #include <vector>
 #include <librealsense2/rs.hpp>
+
 #include <camera_info_manager/camera_info_manager.h>
 #include <image_transport/image_transport.h>
 #include "tku_msgs/camera.h"
 #include "tku_msgs/CameraInfo.h"
 #include "RGB_camera_controls/RGB_camera_controls.h"
+#include "base_realsense_node.h"
+#include "realsense_node_factory.h"
 
-class Camera_control 
+using namespace realsense2_camera;
+
+class Camera_control
 {
     private:
         ros::Subscriber getpara_sub_;
         ros::ServiceServer Camera_service;
+        // rs2::context ctx; 
+        // rs2::device_list devices;
         ros::NodeHandle *nh;
     public:
-        Camera_control(ros::NodeHandle &nh);
+        rs2::sensor sensor; 
+        Camera_control( ros::NodeHandle& nh);
         ~Camera_control();
         float exposure_, brightness_, contrast_, saturation_,white_balance_;
         bool auto_exposure_, auto_white_balance_,auto_Backlight_Compensation_;
-        rs2::sensor sensor; 
         void loadCameraFile();
         void saveCameraFile();
-        void print_device_information();
+        // void print_device_information();
         inline void print_separator();
-        rs2::sensor get_a_sensor_from_a_device(); 
-        void get_sensor_all_option_value();
+        rs2::sensor get_sensor_from_a_device(); 
+        // void get_sensor_all_option_value();
         bool CallCameraInfoFunction(tku_msgs::CameraInfo::Request &req, tku_msgs::CameraInfo::Response &res);
         void SetCameraParameter();
         void getparaCallback(const tku_msgs::camera &msg);
--- realsense2_camera/src/Camera_Controls.cpp
+++ realsense2_camera/src/Camera_Controls.cpp
@@ -2,7 +2,7 @@
 
 //Camera_control* camera_control = new Camera_control(ros::NodeHandle &nh);
 
-Camera_control::Camera_control(ros::NodeHandle &nh)
+Camera_control::Camera_control(ros::NodeHandle& nh)
 {
     this->nh = &nh;
     image_transport::ImageTransport it(nh);    
@@ -55,69 +55,82 @@ void Camera_control::saveCameraFile()
     RGB_camera_controls->SaveCameraSetFile();
 }
 
-void Camera_control::print_device_information()
-{
-    rs2::context ctx; 
-    rs2::device_list devices = ctx.query_devices();
-    rs2::device selected_device = devices[0];
-    std::cout << "Device information: " << std::endl;
-    for (int i = 0; i < static_cast<int>(RS2_CAMERA_INFO_COUNT); i++)
-    {
-        rs2_camera_info info_type = static_cast<rs2_camera_info>(i);
-        std::cout << "  " << std::left << std::setw(20) << info_type << " : ";
-        if (selected_device.supports(info_type))
-            std::cout << selected_device.get_info(info_type) << std::endl;
-        else
-            std::cout << "N/A" << std::endl;
-    }
-}
+// void Camera_control::print_device_information()
+// {
+//     rs2::context ctx; 
+//     rs2::device_list devices = ctx.query_devices();
+//     rs2::device selected_device = devices[0];
+//     std::cout << "Device information: " << std::endl;
+//     for (int i = 0; i < static_cast<int>(RS2_CAMERA_INFO_COUNT); i++)
+//     {
+//         rs2_camera_info info_type = static_cast<rs2_camera_info>(i);
+//         std::cout << "  " << std::left << std::setw(20) << info_type << " : ";
+//         if (selected_device.supports(info_type))
+//             std::cout << selected_device.get_info(info_type) << std::endl;
+//         else
+//             std::cout << "N/A" << std::endl;
+//     }
+// }
 
 inline void Camera_control::print_separator()
 {
     std::cout << "\n======================================================\n" << std::endl;
 }
 
-rs2::sensor Camera_control::get_a_sensor_from_a_device()
+rs2::sensor Camera_control::get_sensor_from_a_device()
 {
     rs2::context ctx; 
-    rs2::device_list devices = ctx.query_devices();
-    rs2::device selected_device = devices[0];
-    std::vector<rs2::sensor> sensors = selected_device.query_sensors();
-    uint32_t selected_sensor_index = 1; //RGB camera
-    return  sensors[selected_sensor_index];
+    rs2::device dev;
+    auto list = ctx.query_devices();
+    if (list.size() == 0)
+    {
+        ROS_WARN("No RealSense devices were found!");
+    }else{
+        try
+        {
+            dev = list[0];
+        }
+        catch(const std::exception& ex)
+        {
+            ROS_WARN_STREAM("Device /" << list.size() << " failed with exception: " << ex.what());
+        }
+        std::vector<rs2::sensor> sensors = dev.query_sensors();
+        //RGB camera
+        return  sensors[1];
+    } 
 }
 
-void Camera_control::get_sensor_all_option_value()
-{
-    std::cout << "Sensor supports the following options:\n" << std::endl;
-    for (int i = 0; i < static_cast<int>(RS2_OPTION_COUNT); i++)
-    {
-        if(i<12)
-        {
-            rs2_option option_type = static_cast<rs2_option>(i);
-            std::cout << "  " << i << ": " << option_type;
-            if (sensor.supports(option_type))
-            {
-                float current_value = sensor.get_option(option_type);
-                rs2::option_range range = sensor.get_option_range(option_type);
-                float default_value = range.def;
-                float maximum_supported_value = range.max;
-                float minimum_supported_value = range.min;
-                float difference_to_next_value = range.step;
-                std::cout << std::endl << "  Current Value : " << current_value << std::endl;
-                std::cout << "  Min Value     : " << minimum_supported_value << std::endl;
-                std::cout << "  Max Value     : " << maximum_supported_value << std::endl;
-                print_separator();
-            }
-            else
-            {
-                std::cout << " is not supported" << std::endl;
-            }
-        }else{
-            break;
-        }
-    }
-}
+// void Camera_control::get_sensor_all_option_value()
+// {
+//     std::cout << "Sensor supports the following options:\n" << std::endl;
+//     for (int i = 0; i < static_cast<int>(RS2_OPTION_COUNT); i++)
+//     {
+//         if(i<12)
+//         {
+//             rs2_option option_type = static_cast<rs2_option>(i);
+//             std::cout << "  " << i << ": " << option_type;
+//             if (sensor.supports(option_type))
+//             {
+//                 float current_value = sensor.get_option(option_type);
+//                 rs2::option_range range = sensor.get_option_range(option_type);
+//                 float default_value = range.def;
+//                 float maximum_supported_value = range.max;
+//                 float minimum_supported_value = range.min;
+//                 float difference_to_next_value = range.step;
+//                 std::cout << std::endl << "  Current Value : " << current_value << std::endl;
+//                 std::cout << "  Min Value     : " << minimum_supported_value << std::endl;
+//                 std::cout << "  Max Value     : " << maximum_supported_value << std::endl;
+//                 print_separator();
+//             }
+//             else
+//             {
+//                 std::cout << " is not supported" << std::endl;
+//             }
+//         }else{
+//             break;
+//         }
+//     }
+// }
 
 bool Camera_control::CallCameraInfoFunction(tku_msgs::CameraInfo::Request &req, tku_msgs::CameraInfo::Response &res)
 {
@@ -133,90 +146,94 @@ bool Camera_control::CallCameraInfoFunction(tku_msgs::CameraInfo::Request &req, 
 
 void Camera_control::SetCameraParameter()
 {
-    rs2_option option_type;
-    if (brightness_ >= -64 )
-    {
-        option_type = static_cast<rs2_option>(1);
-        try
+    // for(rs2::sensor sensor : _dev_sensors_copy)
+    // {
+        rs2_option option_type;
+        if (brightness_ >= -64 )
         {
-            sensor.set_option(option_type, brightness_);
-        }
-        catch (const rs2::error& e)
-        {
-            std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
-        }
-    }
-    if (contrast_ >= 0)
-    {
-        option_type = static_cast<rs2_option>(2);
-        try
-        {
-            sensor.set_option(option_type, contrast_);
-        }
-        catch (const rs2::error& e)
-        {
-            std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
-        }
-    }
-    if (saturation_ >= 0 )
-    {
-        option_type = static_cast<rs2_option>(7);
-        try
-        {
-            sensor.set_option(option_type, saturation_);
-        }
-        catch (const rs2::error& e)
-        {
-            std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
-        }
-    }
-    if(auto_white_balance_ == 0 )
-    {
-        option_type = static_cast<rs2_option>(11);
-        sensor.set_option(option_type, 0);
-        if (white_balance_ >= 2800 )
-        {
-            option_type = static_cast<rs2_option>(9);
+            option_type = static_cast<rs2_option>(1);
             try
             {
-                sensor.set_option(option_type, white_balance_);
+                sensor.set_option(option_type, brightness_);
+            }
+            catch (const rs2::error& e)
+            {
+                std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
+            }
+        }
+        if (contrast_ >= 0)
+        {
+            option_type = static_cast<rs2_option>(2);
+            try
+            {
+                sensor.set_option(option_type, contrast_);
+            }
+            catch (const rs2::error& e)
+            {
+                std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
+            }
+        }
+        if (saturation_ >= 0 )
+        {
+            option_type = static_cast<rs2_option>(7);
+            try
+            {
+                sensor.set_option(option_type, saturation_);
+            }
+            catch (const rs2::error& e)
+            {
+                std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
+            }
+        }
+        if(auto_white_balance_ == 0 )
+        {
+            option_type = static_cast<rs2_option>(11);
+            sensor.set_option(option_type, 0);
+            if (white_balance_ >= 2800 )
+            {
+                option_type = static_cast<rs2_option>(9);
+                try
+                {
+                    sensor.set_option(option_type, white_balance_);
+                }
+                catch (const rs2::error& e)
+                {
+                    std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
+                }  
+            }
+        }else
+        {
+            option_type = static_cast<rs2_option>(11);
+            try
+            {
+                sensor.set_option(option_type, 1);
             }
             catch (const rs2::error& e)
             {
                 std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
             }  
         }
-    }else
-    {
-        option_type = static_cast<rs2_option>(11);
-        try
+        if (auto_exposure_ == 1 )
         {
+            option_type = static_cast<rs2_option>(10);
             sensor.set_option(option_type, 1);
-        }
-        catch (const rs2::error& e)
+        }else
         {
-            std::cerr << "Failed to set option " << option_type << ". (" << e.what() << ")" << std::endl;
-        }  
-    }
-    if (auto_exposure_ == 1 )
-    {
-        option_type = static_cast<rs2_option>(10);
-        sensor.set_option(option_type, 1);
-    }else
-    {
-        option_type = static_cast<rs2_option>(10);
-        sensor.set_option(option_type, 0);
-    }
-    if (auto_Backlight_Compensation_ == 1 )
-    {
-        option_type = static_cast<rs2_option>(0);
-        sensor.set_option(option_type, 1);
-    }else
-    {
-        option_type = static_cast<rs2_option>(0);
-        sensor.set_option(option_type, 0);
-    }
-    saveCameraFile();
+            option_type = static_cast<rs2_option>(10);
+            sensor.set_option(option_type, 0);
+        }
+        if (auto_Backlight_Compensation_ == 1 )
+        {
+            option_type = static_cast<rs2_option>(0);
+            sensor.set_option(option_type, 1);
+        }else
+        {
+            option_type = static_cast<rs2_option>(0);
+            sensor.set_option(option_type, 0);
+        }
+        saveCameraFile();
+    // }
+    
 }
 
 void Camera_control::getparaCallback(const tku_msgs::camera &msg)
@@ -236,6 +253,11 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "realsense_cam");
     ros::NodeHandle nh;
+    // ros::NodeHandle privateNodeHandle;
+    // RealSenseNodeFactory realSenseNodeFactory;
+    // ros::NodeHandle nh = realSenseNodeFactory.getNodeHandle();
+	// auto privateNh = realSenseNodeFactory.getPrivateNodeHandle();
+    
     Camera_control camera_control(nh);
     
     //camera_control->print_device_information();
@@ -245,8 +267,7 @@ int main(int argc, char **argv)
     while (nh.ok())
     {
         //camera_control->print_separator();
-        camera_control.sensor = camera_control.get_a_sensor_from_a_device();
-        camera_control.get_sensor_all_option_value();
+        camera_control.sensor = camera_control.get_sensor_from_a_device();
         ros::spinOnce();
         loop_rate.sleep();
     }
